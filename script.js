76ae9113248bcaa25783a1c2aad80c161360634e
Marin Perak <marin.perak7@gmail.com>
2022-10-01 22:19:58 +0200

    35

--- script.js
+++ script.js
@@ -12,9 +12,11 @@ if (hasDriversLicense) console.log("I can drive :D");
 const if = 23;
 */
 
+/*
 function logger() {
   console.log("My name is Jonas");
 }
+
 
 // calling / running / invoking function
 logger();
@@ -31,3 +33,36 @@ console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
+*/
+
+/*
+// Function declaration
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+
+// Function expression
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
+*/
+
+// Arrow function
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(1991, "Jonas"));
+console.log(yearsUntilRetirement(1980, "Bob"));
